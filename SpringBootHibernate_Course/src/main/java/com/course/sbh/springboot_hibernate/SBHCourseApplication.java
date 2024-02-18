cb927c2871665e75b902564ec47eb5a6dd8b63ec
Kamil Kołtan <40299216+kamkol@users.noreply.github.com>
2024-02-18 14:46:37 +0100

    first commit

---
+++ SpringBootHibernate_Course/src/main/java/com/course/sbh/springboot_hibernate/SBHCourseApplication.java
@@ -0,0 +1,14 @@
+package com.course.sbh.springboot_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class SBHCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SBHCourseApplication.class, args);
+	}
+
+}
---
+++ SpringBootHibernate_Course/src/main/java/rest/FunRestController.java
@@ -0,0 +1,14 @@
+package rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+	
+	//expose "/" that return "Hello world!"
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello world!";
+	}
+}
