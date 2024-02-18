48ad856b019f85e9dafafd42372ed13b3ef33f32
Kamil Kołtan <40299216+kamkol@users.noreply.github.com>
2024-02-18 14:53:38 +0100

    deleting comments

--- SpringBootHibernate_Course/src/main/java/rest/FunRestController.java
+++ SpringBootHibernate_Course/src/main/java/rest/FunRestController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FunRestController {
 	
-	//expose "/" that return "Hello world!"
 	@GetMapping("/")
 	public String sayHello() {
 		return "Hello world!";
