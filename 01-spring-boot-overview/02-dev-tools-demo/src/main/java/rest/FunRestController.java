224fca78e1264272d290c24c55cab2eafebb6e9e
Kamil Kołtan <40299216+kamkol@users.noreply.github.com>
2024-02-21 17:43:46 +0100

    White Label Error

---
+++ 01-spring-boot-overview/02-dev-tools-demo/src/main/java/rest/FunRestController.java
@@ -0,0 +1,19 @@
+package rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+	
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello world!";
+	}
+	
+//	expose new endopint for "workout"
+	@GetMapping("/workout")
+	public String getDailyWorkout() {
+		return "Hello your daily workout!";
+	}
+}
