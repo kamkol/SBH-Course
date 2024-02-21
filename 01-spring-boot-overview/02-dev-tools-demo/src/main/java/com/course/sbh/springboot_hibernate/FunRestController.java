ca4c2fdcbe9c559cddf784d3deb1adc453250f0f
Kamil Kołtan <40299216+kamkol@users.noreply.github.com>
2024-02-21 21:32:18 +0100

    Whitelabel error fixed

--- 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/course/sbh/springboot_hibernate/FunRestController.java
+++ 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/course/sbh/springboot_hibernate/FunRestController.java
@@ -1,4 +1,4 @@
-package rest;
+package com.course.sbh.springboot_hibernate;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
