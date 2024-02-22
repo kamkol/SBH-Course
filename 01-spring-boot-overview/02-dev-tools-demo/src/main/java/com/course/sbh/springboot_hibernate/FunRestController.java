27249dc5411cd408ffca9eef5dad381dcd426efd
Kamil Kołtan <40299216+kamkol@users.noreply.github.com>
2024-02-22 15:17:20 +0100

    FunRestController - GetMapping

--- 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/course/sbh/springboot_hibernate/FunRestController.java
+++ 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/course/sbh/springboot_hibernate/FunRestController.java
@@ -11,9 +11,13 @@ public class FunRestController {
 		return "Hello world!";
 	}
 	
-//	expose new endopint for "workout"
 	@GetMapping("/workout")
 	public String getDailyWorkout() {
 		return "Hello your daily workout!";
+	}
+	
+	@GetMapping("/fortune")
+	public String getDailyFortune() {
+		return "TOday is your lucky day!";
 	}
 }
