5d516ebffe2f7e1ca2c121e218352f1faf0dde8e
Kamil Kołtan <40299216+kamkol@users.noreply.github.com>
2024-02-27 15:49:09 +0100

    03-actuator

---
+++ 01-spring-boot-overview/03-actuator-demo/src/main/java/com/course/sbh/springboot_hibernate/SBHCourseApplication.java
@@ -0,0 +1,13 @@
+package com.course.sbh.springboot_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBHCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SBHCourseApplication.class, args);
+	}
+
+}
