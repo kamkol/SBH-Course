90fe109b519b93c424177872dd0e82187ebf8b08
Kamil Kołtan <40299216+kamkol@users.noreply.github.com>
2024-03-01 15:37:22 +0100

    02-ex2 branch

--- 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/sbh_course/springcoredemo/CricketCoach.java
+++ 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/sbh_course/springcoredemo/CricketCoach.java
@@ -7,7 +7,8 @@ public class CricketCoach implements Coach {
 
 	@Override
 	public String getDailyWorkout() {
-		// TODO Auto-generated method stub
+		
+		
 		return "Practice fast bowling for 15 minutes";
 	}
 
