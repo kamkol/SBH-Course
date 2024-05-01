7a3292f42e28f86882a8204589049f7fc4fa714a
Kamil Kołtan <40299216+kamkol@users.noreply.github.com>
2024-05-01 13:17:32 +0200

    refactor StudentRestController

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/sbh/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/sbh/demo/rest/StudentRestController.java
@@ -9,17 +9,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.sbh.demo.entity.Student;
 
+import jakarta.annotation.PostConstruct;
+
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
 	
-	//define endpoint for "/student" return a list of student
-	@GetMapping("/students")
-	public List<Student> getStudents() {
-		List<Student> theStudents = new ArrayList<Student>();
+	private List<Student> theStudents;
+	
+	//define @PostConstruct to load the student data
+	@PostConstruct
+	public void loadData() {
+		theStudents = new ArrayList<Student>();
+		
 		theStudents.add(new Student("Poornima", "Patel"));
 		theStudents.add(new Student("Josh", "Rossi"));
 		theStudents.add(new Student("Gaby", "Smith"));
+	}
+	
+	//define endpoint for "/student" return a list of student
+	@GetMapping("/students")
+	public List<Student> getStudents() {
 		return theStudents;
 	}
 	
