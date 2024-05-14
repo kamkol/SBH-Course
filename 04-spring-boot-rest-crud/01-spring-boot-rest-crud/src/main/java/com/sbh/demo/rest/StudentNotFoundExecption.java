4fcdea21a715371892954afe0b49a30eea581e23
Kamil Kołtan <40299216+kamkol@users.noreply.github.com>
2024-05-14 16:03:53 +0200

    Exceptions: RuntimeException

---
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/sbh/demo/rest/StudentNotFoundExecption.java
@@ -0,0 +1,16 @@
+package com.sbh.demo.rest;
+
+public class StudentNotFoundExecption extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7879677716033056596L;
+	
+	
+
+}
