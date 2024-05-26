76366920aaaf1e8ca3443ec5609821b3ceefba12
Kamil Kołtan <40299216+kamkol@users.noreply.github.com>
2024-05-26 21:17:53 +0200

    EmployeeService Implementation

---
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/sbhCourse/cruddemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,25 @@
+package com.sbhCourse.cruddemo.service;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+import com.sbhCourse.cruddemo.dao.EmployeeDAO;
+import com.sbhCourse.cruddemo.entity.Employee;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+
+	private EmployeeDAO employeeDAO;
+	
+	public EmployeeServiceImpl(EmployeeDAO theEmployeeDAO) {
+		employeeDAO = theEmployeeDAO;
+	}
+	
+	@Override
+	public List<Employee> findAll() {
+		
+		return null;
+	}
+	
+	
+}
