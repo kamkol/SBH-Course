dcdfebaaec2a6778ac34b0d299df4a647e7ba4e2
Kamil Kołtan <40299216+kamkol@users.noreply.github.com>
2024-08-06 16:49:06 +0200

    add a single employee

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/sbhCourse/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/sbhCourse/cruddemo/rest/EmployeeRestController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.sbhCourse.cruddemo.entity.Employee;
@@ -36,5 +38,28 @@ public class EmployeeRestController {
 		}
 		return thEmployee;
 	}
+	
+	//add mapping for POST /employees - add new employee
+	@PostMapping("/employees/{employeeId}")
+	public Employee addEmployee(@RequestBody Employee theEmployee) {
+		//set id to 0 -> this is to force of new item instead of update
+		theEmployee.setId(0);
+		
+		Employee dbEmployee = employeeService.save(theEmployee);
+		
+		return dbEmployee;
+	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
