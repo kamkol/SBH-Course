cf3ac39a7db8eccb8b161bd5224faafbc2ec1b2a
Kamil Kołtan <40299216+kamkol@users.noreply.github.com>
2024-08-18 13:10:27 +0200

    Creating InMemoryUserDetailsManager and testing users

---
+++ 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -0,0 +1,36 @@
+package com.luv2code.springboot.cruddemo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.User;
+ 
+
+@Configuration
+public class DemoSecurityConfig {
+	
+	@Bean
+	public InMemoryUserDetailsManager userDetailsManager() {
+
+		UserDetails john = User.builder()
+				.username("john")
+				.password("{noop}test123")
+				.roles("EMPLOYEE")
+				.build();
+		
+		UserDetails marry = User.builder()
+				.username("marry")
+				.password("{noop}test123")
+				.roles("EMPLOYEE", "MANAGER")
+				.build();
+				
+		UserDetails susan = User.builder()
+				.username("susan")
+				.password("{noop}test123")
+				.roles("EMPLOYEE", "MANAGER", "ADMIN")
+				.build();
+
+	}
+
+}
