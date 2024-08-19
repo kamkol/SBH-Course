95a8647d1032240ff5cc30b4f77ca2125460e3d0
Kamil Kołtan <40299216+kamkol@users.noreply.github.com>
2024-08-19 15:39:20 +0200

    Adding roles and matchers

--- 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -2,8 +2,11 @@ package com.luv2code.springboot.cruddemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.codec.CodecConfigurer;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
  
 
@@ -34,5 +37,40 @@ public class DemoSecurityConfig {
 		return new InMemoryUserDetailsManager(john, marry, susan);
 
 	}
+	
+	
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		http.authorizeHttpRequests(configurer ->
+			configurer
+				.requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+				.requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+				.requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+				.requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+				.requestMatchers(HttpMethod.DELETE, "/api/employees").hasRole("ADMIN")
+				);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
