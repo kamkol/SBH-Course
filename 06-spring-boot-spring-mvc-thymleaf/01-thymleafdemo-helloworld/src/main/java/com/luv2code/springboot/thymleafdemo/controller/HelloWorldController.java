ca13a7a9350c595993c80f75b95f1931d766536a
Kamil Kołtan <40299216+kamkol@users.noreply.github.com>
2024-09-03 15:48:18 +0200

    controller method to read form data and add data to the model

--- 06-spring-boot-spring-mvc-thymleaf/01-thymleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymleafdemo/controller/HelloWorldController.java
+++ 06-spring-boot-spring-mvc-thymleaf/01-thymleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymleafdemo/controller/HelloWorldController.java
@@ -3,7 +3,6 @@ package com.luv2code.springboot.thymleafdemo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
@@ -23,8 +22,22 @@ public class HelloWorldController {
 	
 	//need a controller method to read form data and
 	//add data to the model
+	//controller method to read form data and add data to the model
 	@RequestMapping("/processFormVersionTwo")
 	public String letsShoutDude(HttpServletRequest request, Model model) {
+		
+		//read the request parameter from the HTML form
+		String theName = request.getParameter("studentName");
+		
+		//convert the data to all caps
+		theName = theName.toUpperCase();
+		
+		//create the message
+		String result = "Yo! " + theName;
+		
+		//add message to the model
+		model.addAttribute("message", result);
+		
 		return "helloworld";
 	}
 
