d6f821cbdf8fdef60beb837f464292fb2a14c8a6
Kamil Kołtan <40299216+kamkol@users.noreply.github.com>
2024-09-03 15:44:44 +0200

    request mappign version two

--- 06-spring-boot-spring-mvc-thymleaf/01-thymleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymleafdemo/controller/HelloWorldController.java
+++ 06-spring-boot-spring-mvc-thymleaf/01-thymleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymleafdemo/controller/HelloWorldController.java
@@ -23,6 +23,7 @@ public class HelloWorldController {
 	
 	//need a controller method to read form data and
 	//add data to the model
+	@RequestMapping("/processFormVersionTwo")
 	public String letsShoutDude(HttpServletRequest request, Model model) {
 		return "helloworld";
 	}
