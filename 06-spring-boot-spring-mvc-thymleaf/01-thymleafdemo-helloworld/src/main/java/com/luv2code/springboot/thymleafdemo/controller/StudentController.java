8ae554248debe97bf457ccd8f9b2cb2d412c2207
Kamil Kołtan <40299216+kamkol@users.noreply.github.com>
2024-09-20 10:25:39 +0200

    student object & form

--- 06-spring-boot-spring-mvc-thymleaf/01-thymleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymleafdemo/controller/StudentController.java
+++ 06-spring-boot-spring-mvc-thymleaf/01-thymleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymleafdemo/controller/StudentController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.luv2code.springboot.thymleafdemo.model.Student;
+
 
 @Controller
 public class StudentController {
@@ -11,7 +13,11 @@ public class StudentController {
 	@GetMapping("/showStudentForm")
 	public String showForm(Model theModel) {
 		
+		//create a student object
+		Student theStudent = new Student();
 		
+		//add student object to the model
+		theModel.addAttribute("student", theStudent);
 		
 		return "student-form";
 	}
